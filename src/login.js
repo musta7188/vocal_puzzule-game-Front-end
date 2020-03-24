0277800946b973f16bf8730fb057cb08ec684c47
Bryn <thenazboz@gmail.com>
2020-03-24 10:01:49 +0000

    up to date, login_sessions

--- src/login.js
+++ src/login.js
@@ -54,22 +54,101 @@ document.querySelector('#submit').addEventListener('focus', function(e) {
 });
 
 
-
+const already = document.querySelector("#already")
 const BASE_URL = "http://localhost:3000"
 const PLAYERS_URL = `${BASE_URL}/players`
-const theLoginForm = document.querySelector("#createForm") 
+const createForm = document.querySelector("#createForm") 
+let theFlag = true
+
+const sessionsUrl = `${BASE_URL}/sessions`
+
+
+
+const changeUiFunction = () => {
+      
+  if (theFlag == true) {
+  const labelName = document.querySelector("#labelName")
+  labelName.innerText = ""
+  labelName.innerText = "Name"
+
+  const labelPassword = document.querySelector("#labelPassword")
+  labelPassword.innerText = ""
+  labelPassword.innerText = "Password"
+
+  const loginButton = document.querySelector("#submit")
+  loginButton.value = ""
+  loginButton.value = "Login"
+
+  const color1 = document.querySelector("#stop876")
+  color1.style = "stop-color:rgb(0, 47, 255)"
+
+  const color2 = document.querySelector("#stop878")
+  color2.style = "stop-color:rgb(52, 255, 25)"
+
+  createForm.id = "loginForm"
+
+  const imageLogPart = document.querySelector("#imageLogPart")
+  const logImg = document.createElement("img")
+  logImg.remove()
+  logImg.src = "https://globalforestatlas.yale.edu/sites/default/files/resize/images/holzstapel-766737_640-350x233.jpg"
+  logImg.alt = "Login"
+  logImg.height = "140"
+  logImg.width = "140"
+  imageLogPart.append(logImg)
+
+  const h2 = document.querySelector("#h2")
+  h2.remove()
+
+  const signUp = document.querySelector("#signUp")
+  signUp.innerText = "🙄 Login to play the speech puzzle  By Mustafa & Bryn"
+   
+  already.remove()
+
+  theFlag = false
+
+//second create listener 
+  const loginForm = document.querySelector("#loginForm")   
+  
+  loginForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const nameInput = event.target.elements.name.value
+    const passwordInput = event.target.elements.password.value
+
+      fetch(sessionsUrl,{
+        method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Accept": "application/json"
+                    },
+                    body: JSON.stringify({
+                      name: (nameInput),
+                      password: (passwordInput)
+                    })
+      })
+      .then(res => res.json())
+      .then(changeUiFunction)
+  })
+
+
+
+}
+  
+}
 
 
 
 
-  theLoginForm.addEventListener("submit", (event) => {
+
+//first create listener 
+  createForm.addEventListener("submit", (event) => {
   event.preventDefault()
    
     const nameInput = event.target.elements.name.value
     const passwordInput = event.target.elements.password.value
-     
-  console.log("submitted")
+    //const changeStyle = document.querySelector("#changeStyle")
 
+    
+  
       fetch(PLAYERS_URL, {
         method: "POST",
                     headers: {
@@ -82,5 +161,12 @@ const theLoginForm = document.querySelector("#createForm")
                     })
       })
       .then(res => res.json())
-      .then(data => console.log(data))     
+      .then(changeUiFunction)
+  })
+
+
+
+  already.addEventListener("click", () => {
+    event.preventDefault()
+    changeUiFunction()
   })
