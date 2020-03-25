014ac9daf5ea792b73188266bf607fb25ee2001b
Bryn <thenazboz@gmail.com>
2020-03-25 09:03:37 +0000

    new front end

--- src/login.js
+++ src/login.js
@@ -1,58 +1,3 @@
-// import anime from './node_modules/animejs/lib/anime.es.js';
-
-// const anime = require('animejs');
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-var current = null;
-document.querySelector('#name').addEventListener('focus', function(e) {
-  if (current) current.pause();
-  current = anime({
-    targets: 'path',
-    strokeDashoffset: {
-      value: 0,
-      duration: 900,
-      easing: 'easeOutQuart'
-    },
-    strokeDasharray: {
-      value: '240 1386',
-      duration: 900,
-      easing: 'easeOutQuart'
-    }
-  });
-});
-document.querySelector('#password').addEventListener('focus', function(e) {
-  if (current) current.pause();
-  current = anime({
-    targets: 'path',
-    strokeDashoffset: {
-      value: -336,
-      duration: 900,
-      easing: 'easeOutQuart'
-    },
-    strokeDasharray: {
-      value: '240 1386',
-      duration: 900,
-      easing: 'easeOutQuart'
-    }
-  });
-});
-document.querySelector('#submit').addEventListener('focus', function(e) {
-  if (current) current.pause();
-  current = anime({
-    targets: 'path',
-    strokeDashoffset: {
-      value: -730,
-      duration: 900,
-      easing: 'easeOutQuart'
-    },
-    strokeDasharray: {
-      value: '530 1386',
-      duration: 900,
-      easing: 'easeOutQuart'
-    }
-  });
-});
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const BASE_URL = "http://localhost:3000"///set the base of the initial url
@@ -61,45 +6,62 @@ const PLAYERS_URL = `${BASE_URL}/players`///get all the players
 
 
  
-const createForm = document.querySelector("#createForm") //select the initial form to create the user 
-let theFlag = true
-const imageLogPart = document.querySelector("#imageLogPart")//image div 
+// const createForm = document.querySelector("#createForm") //select the initial form to create the user 
+ 
+// const imageLogPart = document.querySelector("#imageLogPart")//image div 
 
-const signUp = document.querySelector("#signUp")
-const h2 = document.querySelector("#h2")//sign up phrase
+// const signUp = document.querySelector("#signUp")
+// const h2 = document.querySelector("#h2")//sign up phrase
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ 
 
-createForm.addEventListener("submit", (event) => {
+  // function createPlayer(nameInput){
+  //   data = {
+  //   method: "POST",
+  //               headers: {
+  //                   "Content-Type": "application/json",
+  //                   "Accept": "application/json"
+  //               },
+  //               body: JSON.stringify({
+  //                 name: (nameInput)
+  //               })
+  //   }
+
+  //   return fetch(PLAYERS_URL, data)
+  //   .then(res => res.json())
+  
+  // }
+
+
+
+  // createForm.addEventListener("submit", (event) => {
    
-  event.preventDefault()
- 
-  const nameInput = event.target.elements.name.value
-  const passwordInput = event.target.elements.password.value
-  //const changeStyle = document.querySelector("#changeStyle")
-  createPlayer(nameInput, passwordInput).then(player => {
-    currentUser = player
-  }) //return the new player created 
-
-})
-
-function createPlayer(nameInput, passwordInput){
-data = {
-  method: "POST",
-              headers: {
-                  "Content-Type": "application/json",
-                  "Accept": "application/json"
-              },
-              body: JSON.stringify({
-                name: (nameInput),
-                password: (passwordInput)
-              })
-}
-
-  return fetch(PLAYERS_URL, data)
-  .then(res => res.json())
- 
-}
+  //   event.preventDefault()
+   
+  //   const nameInput = event.target.elements.name.value
+     
+     
+  //   createPlayer(nameInput).then(player => {
+  //     currentUser = player
+  //   }) //return the new player created 
+  
+  // })
 
 
+//submit button event
+  const loginButton = document.querySelector("#loginButton")
 
+  const createCard = () => {
+
+     
+
+
+  }
+
+  loginButton.addEventListener("submit", (event) => {
+    event.preventDefault()
+    debugger
+    console.log("hi")
+  })
+   
--- src/test.js
+++ src/test.js
@@ -1,74 +1,74 @@
 
-const changeUiFunction = () => {  
-  if (theFlag == true) {
+// const changeUiFunction = () => {  
+//   if (theFlag == true) {
 
-  createForm.id = "loginForm" ;
+//   createForm.id = "loginForm" ;
 
-  changeColorFormAnimation();
+//   changeColorFormAnimation();
 
-  fromSignUpToSignIn();
+//   fromSignUpToSignIn();
 
-//second create listener 
-  const loginForm = document.querySelector("#loginForm")   
+// //second create listener 
+//   const loginForm = document.querySelector("#loginForm")   
   
-  loginForm.addEventListener("submit", (event) => {
-    debugger
-    event.preventDefault()
-    const nameInput = event.target.elements.name.value
-    const passwordInput = event.target.elements.password.value
-    newPlayer(nameInput, passwordInput)
+//   loginForm.addEventListener("submit", (event) => {
+//     debugger
+//     event.preventDefault()
+//     const nameInput = event.target.elements.name.value
+//     const passwordInput = event.target.elements.password.value
+//     newPlayer(nameInput, passwordInput)
       
-  })
-} 
-}
+//   })
+// } 
+// }
 
-function newPlayer(nameInput, passwordInput) {
+// function newPlayer(nameInput, passwordInput) {
 
-  const data = {
-    method: "POST",
-     headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-     },
-    body: JSON.stringify({
-       name: (nameInput),
-      password: (passwordInput)
-     })
-  }
+//   const data = {
+//     method: "POST",
+//      headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//      },
+//     body: JSON.stringify({
+//        name: (nameInput),
+//       password: (passwordInput)
+//      })
+//   }
 
-  fetch(sessionsUrl, data)
-  .then(res => res.json())
-  .then(changeUiFunction)///return a new player session
-}
+//   fetch(sessionsUrl, data)
+//   .then(res => res.json())
+//   .then(changeUiFunction)///return a new player session
+// }
 
-function changeColorFormAnimation(){
-  const color1 = document.querySelector("#stop876") //change color to the log in choice 
-  color1.style = "stop-color:rgb(0, 47, 255)"
+// function changeColorFormAnimation(){
+//   const color1 = document.querySelector("#stop876") //change color to the log in choice 
+//   color1.style = "stop-color:rgb(0, 47, 255)"
 
-  const color2 = document.querySelector("#stop878") //change color to the log in choice 
-  color2.style = "stop-color:rgb(52, 255, 25)"
+//   const color2 = document.querySelector("#stop878") //change color to the log in choice 
+//   color2.style = "stop-color:rgb(52, 255, 25)"
 
-  }
-function fromSignUpToSignIn() {
-  h2.innerHTML = "Log In"
+//   }
+// function fromSignUpToSignIn() {
+//   h2.innerHTML = "Log In"
 
-  signUp.innerHTML = "Log In to play the speech puzzle By Mustafa & Bryn"
+//   signUp.innerHTML = "Log In to play the speech puzzle By Mustafa & Bryn"
    
-  already.innerHTML = "you do not have an account yet? Sign Up"///add event listener
+//   already.innerHTML = "you do not have an account yet? Sign Up"///add event listener
 
-  return theFlag = false
-  }
-function createImage(){
-  const logImg = document.createElement("img")
-   logImg.remove()
-  logImg.src = "https://globalforestatlas.yale.edu/sites/default/files/resize/images/holzstapel-766737_640-350x233.jpg"
-  logImg.alt = "Login"
-  logImg.height = "140"
-  logImg.width = "140"
-  imageLogPart.append(logImg)//append to the div for the image the new image created 
- }
+//   return theFlag = false
+//   }
+// function createImage(){
+//   const logImg = document.createElement("img")
+//    logImg.remove()
+//   logImg.src = "https://globalforestatlas.yale.edu/sites/default/files/resize/images/holzstapel-766737_640-350x233.jpg"
+//   logImg.alt = "Login"
+//   logImg.height = "140"
+//   logImg.width = "140"
+//   imageLogPart.append(logImg)//append to the div for the image the new image created 
+//  }
 
- already.addEventListener("click", () => {
-    event.preventDefault()
-    changeUiFunction()
-  })
+//  already.addEventListener("click", () => {
+//     event.preventDefault()
+//     changeUiFunction()
+//   })
